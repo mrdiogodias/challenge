26e588f0423eed3e78910d0daaa5b8d25232bfe6
Diogo Dias <diogo.dias@ceiia.com>
2025-02-16 22:46:46 +0000

    Create out of tree driver

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 
 int main(void) {
     int ret = 0;
-    const struct device *const dev = DEVICE_DT_GET_ONE(bosch_bmi270);
+    const struct device *const dev = DEVICE_DT_GET_ONE(bosch_bmi270plus);
     struct sensor_value acc[3], gyr[3];
     struct sensor_value full_scale, sampling_freq, oversampling;
 
